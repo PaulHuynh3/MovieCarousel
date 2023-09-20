582cebe0bda9b85f912ee6a10c2dc2a2eadcf75a
Paul <paul.huynh3@gmail.com>
2023-09-20 14:43:31 -0400

    Initial Commit

---
+++ MovieCarousel/MovieCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCarouselApp.swift
+//  MovieCarousel
+//
+//  Created by Paul Huynh on 2023-09-20.
+//
+
+import SwiftUI
+
+@main
+struct MovieCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
