aabfcc9782e60b7356f83840f2ee4c46a5fec90f
Paul <paul.huynh3@gmail.com>
2023-09-20 21:34:27 -0400

    create navigationView and scrollView inside HStacks

--- MovieCarousel/ContentView.swift
+++ MovieCarousel/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(0..<20) { num in
+                            Text(num.description)
+                                .padding()
+                                .background(.red)
+                        }
+                    }
+                }
+                
+            }.navigationTitle("Movies Carousel")
         }
-        .padding()
     }
 }
 
